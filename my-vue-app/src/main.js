798e1c6f57e43a3a1c3685e820f250de4232e446
Aldin Sakic <aldinsakic@duck.com>
2024-06-29 03:00:33 +0200

    working on the landing page
    just playing around with vue

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -23,9 +23,9 @@ const createWindow = () => {
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
-  mainWindow.removeMenu();
+  // mainWindow.removeMenu();
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
